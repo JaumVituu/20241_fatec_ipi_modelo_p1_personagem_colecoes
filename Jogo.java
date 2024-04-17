8aeb448728a78538b9049c0905539130ab79e11c
JaumVituu <78977800+JaumVituu@users.noreply.github.com>
2024-04-17 15:33:17 -0300

    feat(p1): adiciona segundo personagem

--- Jogo.java
+++ Jogo.java
@@ -4,9 +4,10 @@ public class Jogo {
     public static void main(String[] args) throws Exception {
         var gerador = new Random();
         var p1 = new Personagem("Cristiano Ronaldo", 10, 9, 0);
+        var p2 = new Personagem("Lionel Messi", 8, 5, 5);
         int oQueFazer;
         
-        while(!p1.morreu()){
+        while(!p1.morreu() && !p2.morreu()){
             oQueFazer = gerador.nextInt(3);
             switch(oQueFazer){
                 case 0:
@@ -21,11 +22,31 @@ public class Jogo {
             }
             System.out.println(p1);
             System.out.printf("\n");
-            Thread.sleep(1000);
-        }
 
+            oQueFazer = gerador.nextInt(3);
+            switch(oQueFazer){
+                case 0:
+                    p2.comer();
+                    break;
+                case 1:
+                    p2.cacar();
+                    break;
+                default:
+                    p2.dormir();
+                    break;
+            }
+            System.out.println(p2);
+            System.out.printf("\n\n-----------------------------\n\n");
+
+            Thread.sleep(500);
+        }
         if(p1.morreu()){
             System.out.println(p1.nome + " morreu...");
+            System.out.println(p2.nome + " foi o sobrevivente!");
+        }
+        else{
+            System.out.println(p2.nome + " morreu...");
+            System.out.println(p1.nome + " foi o sobrevivente!");
         }   
     }
 }
